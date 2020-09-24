a5390dbf1e973676351dec4e426b4d101d85d039
hananbarham99 <1172605@student.birzeit.edu>
2020-09-24 21:08:37 +0300

    eample2

---
+++ app/src/main/java/hanan/birzeit/comp438/example/examle22/MainActivity.java
@@ -0,0 +1,24 @@
+package hanan.birzeit.comp438.example.examle22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+private Spinner spinner;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        spinner = findViewById(R.id.spinner);
+    }
+
+    public void btnSubmitOnClick(View view) {
+        String item = spinner.getSelectedItem().toString();
+        Toast.makeText(this, item, Toast.LENGTH_SHORT).show();
+
+    }
+}
